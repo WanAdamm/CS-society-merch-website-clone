bb978204de7fbf40770efef183e7e2f39a74647b
WanAdamm <wanadamworkmail@gmail.com>
2023-11-06 19:40:09 +0800

    image carousel figured out, footer done, site responsive

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var glider_list = document.querySelectorAll('.glider'); // returns NodeList
+var glider_array = [...glider_list]; // converts NodeList to Array
+
+var dots_list = document.querySelectorAll('#dots'); // returns NodeList
+var dots_array = [...dots_list];
+
+glider_array.forEach((glider,index) => {
+
+    const dots = dots_array[index];
+
+    new Glider(glider, {
+    slideToShow: 1,
+    draggable: true,
+    dots: dots,
+    });
+
+});
+
